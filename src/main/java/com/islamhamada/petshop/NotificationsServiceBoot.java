caed2d9cb478b8b5c43488f70b0378cf6c1d2c33
IslamHamada <islamhamada222@gmail.com>
2026-02-04 14:44:25 +0100

    add main to boot spring

---
+++ src/main/java/com/islamhamada/petshop/NotificationsServiceBoot.java
@@ -0,0 +1,12 @@
+package com.islamhamada.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsServiceBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsServiceBoot.class, args);
+	}
+}
