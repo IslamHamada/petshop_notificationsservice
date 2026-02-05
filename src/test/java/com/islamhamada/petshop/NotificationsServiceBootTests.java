e014db0967d294f2a5b29a9fc8d06d9d047848c1
IslamHamada <islamhamada222@gmail.com>
2026-02-05 21:11:27 +0100

    add context test

---
+++ src/test/java/com/islamhamada/petshop/NotificationsServiceBootTests.java
@@ -0,0 +1,13 @@
+package com.islamhamada.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsServiceBootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
